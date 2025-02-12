a77a51b0bfe731d3ed08b0b1a57f6bc7fd2929a7
peter wafula <peterwaf@gmail.com>
2025-02-12 19:18:07 +0300

    update states

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import DisplayCounter from "./components/displayCounter";
+import "./App.css";
+function App() {
+  return (
+    <>
+      <div id="container-content">
+        <DisplayCounter />
+      </div>
+    </>
+  );
+}
+
+export default App;
